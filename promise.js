21567efda5effa060794ce6d7167d4363b8a9285
aleksdavtyan <davtyan.it@gmail.com>
2021-03-18 19:28:14 +0400

    hw18 1st, 2nd task commit.

--- promise.js
+++ promise.js
@@ -1,14 +1,31 @@
 // 1.
 const random = require('random');
-const promise = new Promise(function (resolve, reject) {
-    const randomNum = random.int(0, 10);
-    console.log(randomNum);
-    if (randomNum < 5) {
-        reject(new Error('the Number is small than 5.'));
-    }else {
-        resolve(randomNum);
-    }
-});
+const http = require('http');
+const fs = require('fs');
+// const promise = new Promise(function (resolve, reject) {
+//     const randomNum = random.int(0, 10);
+//     console.log(randomNum);
+//     if (randomNum < 5) {
+//         reject(new Error('the Number is small than 5.'));
+//     }else {
+//         resolve(randomNum);
+//     }
+// });
 
 // 2.
+fs.readFile('config.json', function (err, param) {
+    if (err) {
+        return err.message;
+    }
+    const serverParams = JSON.parse(param.toString());
+    const host = serverParams.host;
+    const port = serverParams.port;
+   const server = http.createServer(function (req, res) {
+        res.write('Hello from Server Created!'); //write a response to the client
+        res.end(); //end the response
+    });
+   server.listen(port, host);
 
+});
+
+// 3.
