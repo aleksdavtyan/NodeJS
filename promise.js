4a649220be6372c1cd5d7bd8e07a2faf7ecbb53e
aleksdavtyan <davtyan.it@gmail.com>
2021-03-18 20:24:04 +0400

    hw18 1st, 2nd, 3rd task commit.

--- promise.js
+++ promise.js
@@ -2,6 +2,7 @@
 const random = require('random');
 const http = require('http');
 const fs = require('fs');
+const fsp = require('fs/promises');
 // const promise = new Promise(function (resolve, reject) {
 //     const randomNum = random.int(0, 10);
 //     console.log(randomNum);
@@ -13,19 +14,35 @@ const fs = require('fs');
 // });
 
 // 2.
-fs.readFile('config.json', function (err, param) {
-    if (err) {
-        return err.message;
-    }
-    const serverParams = JSON.parse(param.toString());
-    const host = serverParams.host;
-    const port = serverParams.port;
-   const server = http.createServer(function (req, res) {
-        res.write('Hello from Server Created!'); //write a response to the client
-        res.end(); //end the response
-    });
-   server.listen(port, host);
-
-});
+// const server = http.createServer(function (req, res) {
+//     res.write('Hello from Server Created!'); //write a response to the client
+//     res.end(); //end the response
+// });
+// fs.readFile('./config.json', function (err, param) {
+//     if (err) {
+//         return err.message;
+//     }
+//     const serverParams = JSON.parse(param.toString());
+//     const host = serverParams.host;
+//     const port = serverParams.port;
+//     server.listen(port, host);
+//
+// });
 
 // 3.
+async function readWriteParts() {
+    const inputData = await fsp.readFile('./input.txt', 'utf-8');
+    const firstPart = inputData.slice(0, inputData.lenght / 2);
+    const secondPart = inputData.slice(0, inputData.lenght / 2);
+
+    await Promise.all([
+        fsp.writeFile('output1.txt', firstPart),
+        fsp.writeFile('output2.txt', secondPart)
+    ]);
+}
+
+readWriteParts().then(function (data) {
+    console.log("Read and Created.");
+}).catch(function (error) {
+    console.log(error);
+});
