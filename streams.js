7faa26d5822e89db751b60ce5fa98dc729d82540
aleksdavtyan <davtyan.it@gmail.com>
2021-03-21 23:48:55 +0400

    task1 update

--- streams.js
+++ streams.js
@@ -1,4 +1,11 @@
 const Stream = require('stream');
 const fs = require('fs');
 
-const readStream = fs.createReadStream('input.txt');
+const readStream = fs.createReadStream('input.txt', {
+    highWaterMark: 10,
+    encoding: 'utf-8'
+});
+
+readStream.on('data', (chunk => {
+    console.log(chunk.replace(/[^a-zA-Z0-9\s\n]/g, ''));
+}));
