dee549253bf4a07d481a49478af6f91133ee05d3
aleksdavtyan <davtyan.it@gmail.com>
2021-03-23 19:27:05 +0400

    task-1, task-2, task-3 commit

---
+++ files-task3.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+
+fs.open('./homework3.txt', 'r+', (err, openedFile) => {
+    const buffer = Buffer.alloc(65536);
+    fs.read(openedFile, buffer, 0, buffer.length, 0, (err, bytesRead) => {
+
+
+        console.log(bytesRead);
+        console.log(buffer.slice(0, bytesRead.toString()));
+    });
+    fs.write(openedFile, buffer, buffer.slice(0, bytesRead), 0, buffer.slice(0, bytesRead).length);
+});
+
+
+// class RemoveSpecialChars extends Transform {
+//     _transform(chunk, encoding, callback) {
+//         this.push(chunk.toString().replace(regExp, ''));
+//         callback();
+//     }
+// }
+// const transform = new RemoveSpecialChars();
+// const readStream = fs.createReadStream('./homeworkr5.txt');
+// const writeStream = fs.createWriteStream('./homeworkw5.txt');
+//
+// readStream.pipe(transform).pipe(writeStream);
