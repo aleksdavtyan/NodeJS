2a8c4907e9d99a86afae5c483e5690a54eb2c61f
aleksdavtyan <davtyan.it@gmail.com>
2021-03-27 19:23:31 +0400

    users route commit

---
+++ users-route.js
@@ -0,0 +1,3 @@
+// Ստեղծել express framework-ով web aplication:
+// Ստեղծել /users route , որին կարող են կատարել GET, POST , PUT և DETELE մեթոդներով request-ներ։
+// Բոլորի դեպքում վերադարձնում որպես response ժամը և մեթոդի անունը։
